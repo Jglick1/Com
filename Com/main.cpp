ce6065b9042e27fa8627f8e11534abe822ff4a2c
Jonah Glick <jonahglick@Jonahs-MacBook-Pro.local>
2018-06-20 22:29:38 -0500

    Initial Commit

---
+++ Com/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Com
+//
+//  Created by Jonah Glick on 6/20/18.
+//  Copyright © 2018 Jonah Glick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
